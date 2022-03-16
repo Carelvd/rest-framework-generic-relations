55657332f6db7851fc68b16eba09147fe39fca8b
Carel <carelvdam@gmail.com>
2022-03-16 09:10:46 +0200

    Update utils.py

--- generic_relations/utils.py
+++ generic_relations/utils.py
@@ -4,8 +4,4 @@ from django.utils.module_loading import import_string
 class ModelSerializerMap(UserDict): # Ideally dict should be subclassed
 
     def __getitem__(self, item):
-        # print(item in self.data)
-        # print(item, self.data.items())
-        # print({import_string(k): import_string(v)() for k, v in self.data.items()})
-        # print(item in (import_string(k) for k in self.data.keys()), [import_string(k) for k in self.data.keys()])
         return {k: import_string(v)() for k, v in self.data.items()}.get(item,None)
